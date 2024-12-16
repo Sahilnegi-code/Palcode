75f795e871be3f0fc876d0a19cedadac9e1bef2f
Sahil Negi <71814728+Sahilnegi-code@users.noreply.github.com>
2024-12-16 22:19:08 +0530

    Update api.js

--- palcode/src/api/api.js
+++ palcode/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://palcode.onrender.com'
 })
 export const googleAuth =  (code) =>{
     console.log(code);
